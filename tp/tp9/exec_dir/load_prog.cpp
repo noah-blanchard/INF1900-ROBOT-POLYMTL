6aeb3adc6cd6a168bd6ce655b805fd4e6cd989c3
noah-blanchard <matanaworgen@gmail.com>
2023-10-31 23:46:54 -0400

    test timer class !

--- tp/tp9/exec_dir/load_prog.cpp
+++ tp/tp9/exec_dir/load_prog.cpp
@@ -1,17 +1,39 @@
 #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
-#include "ByteCodeInterpreter.h"
-#include "Debug.h"
+// interrupt
+#include <avr/interrupt.h>
+#include "LED.h"
+
+volatile bool flag = false;
+
+ISR(TIMER1_COMPA_vect)
+{
+  flag = true;
+}
 
 int main()
 {
-  ByteCodeInterpreter interpreter;
-  //LED led(&PORTA, &DDRA, PA0, PA1);
-  //interpreter.receiveAndSave();
+  LED led(&PORTA, &DDRA, PA0, PA1);
+
+  TimerConfig timerConfig;
+  timerConfig.timer = 1;
+  timerConfig.prescaler = 1024;
+  timerConfig.delay_ms = 1000;
+  Timer timer = Timer(timerConfig);
+
+  timer.enable();
+
   while (1)
   {
-    interpreter.run();
+    if (flag)
+    {
+      led.turnLedGreen();
+    }
+    else
+    {
+      led.turnLedRed();
+    }
   }
   return 0;
 }
