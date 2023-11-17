ce085b99a095f1f6530627a9c230d9da8c2309bf
noah-blanchard <matanaworgen@gmail.com>
2023-11-17 16:41:15 -0500

    hduzé é

--- tp/projet/lib_dir/IdentifyCorner.cpp
+++ tp/projet/lib_dir/IdentifyCorner.cpp
@@ -10,7 +10,8 @@ IdentifyCorner::~IdentifyCorner()
 
 void IdentifyCorner::identificationProcess(uint8_t *_beginning)
 {
-    _display = "Searching for corner...";
+    _display.clear();
+    _display << "Searching for corner...";
     while (!_found)
     {
         switch (_state)
@@ -19,34 +20,23 @@ void IdentifyCorner::identificationProcess(uint8_t *_beginning)
             _goForward();
             break;
 
-        case IdentifyCornerState::DETECT_INTERSECTION:
-            _detectIntersection();
+        case IdentifyCornerState::TURN_AROUND:
+            _turnAround();
             break;
 
-        case IdentifyCornerState::DETECT_FORWARD:
-            _detectForward();
-            break;
-
-        case IdentifyCornerState::TURN:
-            _turn();
-            break;
-
-        case IdentifyCornerState::RECOGNIZE:
-            _matchSequence();
-            break;
-
-        case IdentifyCornerState::STOP:
-            //_stop();
+        case IdentifyCornerState::GO_BACK:
+            //_goBack();
             break;
         }
     }
     return;
 }
 
-// go forward should follow the line using the line maker module
+// go forward should follow the line using the line maker module until no line is detected
 void IdentifyCorner::_goForward()
 {
-    _display = "GO FORWARD";
+    _display.clear();
+    _display << "GO FORWARD";
     LineMakerFlag flag = _lineMakerModule.getDetectionFlag();
     switch (flag)
     {
@@ -63,222 +53,311 @@ void IdentifyCorner::_goForward()
         break;
 
     case LineMakerFlag::RIGHT_CROSSROAD:
-        _navModule.stop();
-        _state = IdentifyCornerState::DETECT_INTERSECTION;
+        _intersectionCount++;
+        isRight = true;
         break;
 
     case LineMakerFlag::LEFT_CROSSROAD:
-        _navModule.stop();
-        _state = IdentifyCornerState::DETECT_INTERSECTION;
+        _intersectionCount++;
+        isRight = false;
         break;
 
     case LineMakerFlag::FULL_CROSSROAD:
+        _intersectionCount++;
+        break;
+    case LineMakerFlag::NO_LINE:
         _navModule.stop();
-        _state = IdentifyCornerState::DETECT_INTERSECTION;
+        _state = IdentifyCornerState::TURN_AROUND;
         break;
     }
+    _displayCurrentIntersectionCount();
 }
 
-// detect intersection should detect the intersection
-void IdentifyCorner::_detectIntersection()
+void IdentifyCorner::_displayCurrentIntersectionCount()
 {
-    _display = "DETECT INTERSECT";
-    _delay_ms(2500);
-    LineMakerFlag flag = _lineMakerModule.getDetectionFlag();
-    uint8_t sequenceToAdd = 0;
-    // use bitshift to add at the right place
-    switch (flag)
-    {
-    case LineMakerFlag::RIGHT_CROSSROAD:
-        sequenceToAdd |= (RIGHT);
-        _display = "RIGHT DETECT";
-        _delay_ms(2500);
-        break;
-
-    case LineMakerFlag::LEFT_CROSSROAD:
-        sequenceToAdd |= (LEFT);
-        _display = "LEFT DETECT";
-        _delay_ms(2500);
-        break;
-
-    case LineMakerFlag::FULL_CROSSROAD:
-        sequenceToAdd |= (LEFT | RIGHT);
-        _display = "FULL DETECT";
-        _delay_ms(2500);
-        break;
-    }
-    _currentSequence |= (sequenceToAdd);
-    _state = IdentifyCornerState::DETECT_FORWARD;
-}
-
-// detect forward should detect the forward
-void IdentifyCorner::_detectForward()
-{
-    // go a bit forward, then wait
-    _navModule.go(120, false);
-    _delay_ms(1000);
-    _navModule.stop();
-    _display = "LOOKING FORWARD";
-    _delay_ms(2500);
-    LineMakerFlag flag = _lineMakerModule.getDetectionFlag();
-    uint8_t sequenceToAdd = 0;
-    // if flag == no line then there's no forward
-    if (!(flag == LineMakerFlag::NO_LINE))
-    {
-        sequenceToAdd |= (FORWARD);
-        _display = "FORWARD DETECTED";
-        _delay_ms(2500);
-    }
-    _currentSequence |= (sequenceToAdd);
-    // add the current sequence into th _sequence with tteh bitshift than increment
-    _sequence |= (_currentSequence << _bitshift);
-    _bitshift += 3;
-
-    // if there's forward in the sequence, just go back to go forward
-    if ((_currentSequence & FORWARD) != 0)
-    {
-        _state = IdentifyCornerState::RECOGNIZE;
-    }
-    else
-    {
-        // if there's no forward, then we need to turn
-        _state = IdentifyCornerState::TURN;
-    }
-}
-
-// turn should turn
-void IdentifyCorner::_turn()
-{
-
-    // determine if left or right using the current sequence
-    bool isLeft = false;
-    if ((_currentSequence & LEFT) != 0)
-    {
-        isLeft = true;
-    }
-    else if ((_currentSequence & RIGHT) != 0)
-    {
-        isLeft = false;
-    }
-
-    // if (isLeft)
-    // {
-    //     _navModule.goRightWheel(120, false);
-    //     _navModule.goLeftWheel(120, true);
-    // }
-    // else
-    // {
-    //     _navModule.goRightWheel(120, true);
-    //     _navModule.goLeftWheel(120, false);
-    // }
-
-    // turn for 250 ms in the right direction
-    if (isLeft)
-    {
-        _navModule.goRightWheel(120, false);
-        _navModule.goLeftWheel(120, true);
-        _delay_ms(250);
-    }
-    else
-    {
-        _navModule.goRightWheel(120, true);
-        _navModule.goLeftWheel(120, false);
-        _delay_ms(250);
-    }
-
-    // stop
-    _navModule.stop();
-
-    _display = "TURNING";
-
-    // now turn until the line is found (left adjustment for right turn, right adjustment for left turn)
-    if (isLeft)
-    {
-        _navModule.goRightWheel(120, false);
-        _navModule.goLeftWheel(120, true);
-        while (_lineMakerModule.getDetectionFlag() != LineMakerFlag::RIGHT_ADJUSTMENT)
-        {
-        }
-    }
-    else
-    {
-        _navModule.goRightWheel(120, true);
-        _navModule.goLeftWheel(120, false);
-        while (_lineMakerModule.getDetectionFlag() != LineMakerFlag::LEFT_ADJUSTMENT)
-        {
-        }
-    }
-
-    // stop
-    _navModule.stop();
-
-    _currentSequence = 0;
-    _state = IdentifyCornerState::RECOGNIZE;
-}
-
-void IdentifyCorner::_matchSequence()
-{
-    _display = "MATCHING";
-    _delay_ms(4000);
-    // if the sequence is the same as the one we're looking for, then we found it
-    if (_sequence == LCTH)
-    {
-        _display = "LCTH";
-        _found = true;
-    }
-    else if (_sequence == LCBV)
-    {
-        _display = "LCBV";
-        _found = true;
-    }
-    else if (_sequence == LCBH)
-    {
-        _display = "LCBH";
-        _found = true;
-    }
-    else if (_sequence == LCTV)
-    {
-        _display = "LCTV";
-        _found = true;
-    }
-    else if (_sequence == RCTH)
-    {
-        _display = "RCTH";
-        _found = true;
-    }
-    else if (_sequence == RCTV)
-    {
-        _display = "RCTV";
-        _found = true;
-    }
-    else if (_sequence == RCBV)
-    {
-        _display = "RCBV";
-        _found = true;
-    }
-    else if (_sequence == RCBH)
-    {
-        _display = "RCBH";
-        _found = true;
-    }
-    else
-    {
-        _display = "NOT FOUND";
-        _found = false;
-    }
-    _delay_ms(4000);
-    _state = IdentifyCornerState::GO_FORWARD;
-}
-
-void IdentifyCorner::_displayCurrentSequence()
-{
-    // display the current sequence
     char displayString[16];
-    // sprintf(displayString, "%d", _currentSequence);
-    //  use sprint f to format as bits display not decim
-
-    // display in base 2 (binary) not decimal
-    sprintf(displayString, "%d", _currentSequence);
-
-    _display = displayString;
+    sprintf(displayString, "%d", _intersectionCount);
+    _display.clear();
+    _display << displayString;
 }
+
+// turn around should turn around
+void IdentifyCorner::_turnAround()
+{
+    _display = "TURN AROUND";
+}
+
+// void IdentifyCorner::identificationProcess(uint8_t *_beginning)
+// {
+//     _display = "Searching for corner...";
+//     while (!_found)
+//     {
+//         switch (_state)
+//         {
+//         case IdentifyCornerState::GO_FORWARD:
+//             _goForward();
+//             break;
+
+//         case IdentifyCornerState::DETECT_INTERSECTION:
+//             _detectIntersection();
+//             break;
+
+//         case IdentifyCornerState::DETECT_FORWARD:
+//             _detectForward();
+//             break;
+
+//         case IdentifyCornerState::TURN:
+//             _turn();
+//             break;
+
+//         case IdentifyCornerState::RECOGNIZE:
+//             _matchSequence();
+//             break;
+
+//         case IdentifyCornerState::STOP:
+//             //_stop();
+//             break;
+//         }
+//     }
+//     return;
+// }
+
+// // go forward should follow the line using the line maker module
+// void IdentifyCorner::_goForward()
+// {
+//     _display = "GO FORWARD";
+//     LineMakerFlag flag = _lineMakerModule.getDetectionFlag();
+//     switch (flag)
+//     {
+//     case LineMakerFlag::NO_ADJUSTMENT:
+//         _navModule.go(120, false);
+//         break;
+
+//     case LineMakerFlag::RIGHT_ADJUSTMENT:
+//         _navModule.adjustRight();
+//         break;
+
+//     case LineMakerFlag::LEFT_ADJUSTMENT:
+//         _navModule.adjustLeft();
+//         break;
+
+//     case LineMakerFlag::RIGHT_CROSSROAD:
+//         _navModule.stop();
+//         _state = IdentifyCornerState::DETECT_INTERSECTION;
+//         break;
+
+//     case LineMakerFlag::LEFT_CROSSROAD:
+//         _navModule.stop();
+//         _state = IdentifyCornerState::DETECT_INTERSECTION;
+//         break;
+
+//     case LineMakerFlag::FULL_CROSSROAD:
+//         _navModule.stop();
+//         _state = IdentifyCornerState::DETECT_INTERSECTION;
+//         break;
+//     }
+// }
+
+// // detect intersection should detect the intersection
+// void IdentifyCorner::_detectIntersection()
+// {
+//     _display = "DETECT INTERSECT";
+//     _delay_ms(2500);
+//     LineMakerFlag flag = _lineMakerModule.getDetectionFlag();
+//     uint8_t sequenceToAdd = 0;
+//     // use bitshift to add at the right place
+//     switch (flag)
+//     {
+//     case LineMakerFlag::RIGHT_CROSSROAD:
+//         sequenceToAdd |= (RIGHT);
+//         _display = "RIGHT DETECT";
+//         _delay_ms(2500);
+//         break;
+
+//     case LineMakerFlag::LEFT_CROSSROAD:
+//         sequenceToAdd |= (LEFT);
+//         _display = "LEFT DETECT";
+//         _delay_ms(2500);
+//         break;
+
+//     case LineMakerFlag::FULL_CROSSROAD:
+//         sequenceToAdd |= (LEFT | RIGHT);
+//         _display = "FULL DETECT";
+//         _delay_ms(2500);
+//         break;
+//     }
+//     _currentSequence |= (sequenceToAdd);
+//     _state = IdentifyCornerState::DETECT_FORWARD;
+// }
+
+// // detect forward should detect the forward
+// void IdentifyCorner::_detectForward()
+// {
+//     // go a bit forward, then wait
+//     _navModule.go(120, false);
+//     _delay_ms(1000);
+//     _navModule.stop();
+//     _display = "LOOKING FORWARD";
+//     _delay_ms(2500);
+//     LineMakerFlag flag = _lineMakerModule.getDetectionFlag();
+//     uint8_t sequenceToAdd = 0;
+//     // if flag == no line then there's no forward
+//     if (!(flag == LineMakerFlag::NO_LINE))
+//     {
+//         sequenceToAdd |= (FORWARD);
+//         _display = "FORWARD DETECTED";
+//         _delay_ms(2500);
+//     }
+//     _currentSequence |= (sequenceToAdd);
+//     // add the current sequence into th _sequence with tteh bitshift than increment
+//     _sequence |= (_currentSequence << _bitshift);
+//     _bitshift += 3;
+
+//     // if there's forward in the sequence, just go back to go forward
+//     if ((_currentSequence & FORWARD) != 0)
+//     {
+//         _state = IdentifyCornerState::RECOGNIZE;
+//     }
+//     else
+//     {
+//         // if there's no forward, then we need to turn
+//         _state = IdentifyCornerState::TURN;
+//     }
+// }
+
+// // turn should turn
+// void IdentifyCorner::_turn()
+// {
+
+//     // determine if left or right using the current sequence
+//     bool isLeft = false;
+//     if ((_currentSequence & LEFT) != 0)
+//     {
+//         isLeft = true;
+//     }
+//     else if ((_currentSequence & RIGHT) != 0)
+//     {
+//         isLeft = false;
+//     }
+
+//     // if (isLeft)
+//     // {
+//     //     _navModule.goRightWheel(120, false);
+//     //     _navModule.goLeftWheel(120, true);
+//     // }
+//     // else
+//     // {
+//     //     _navModule.goRightWheel(120, true);
+//     //     _navModule.goLeftWheel(120, false);
+//     // }
+
+//     // turn for 250 ms in the right direction
+//     if (isLeft)
+//     {
+//         _navModule.goRightWheel(120, false);
+//         _navModule.goLeftWheel(120, true);
+//         _delay_ms(250);
+//     }
+//     else
+//     {
+//         _navModule.goRightWheel(120, true);
+//         _navModule.goLeftWheel(120, false);
+//         _delay_ms(250);
+//     }
+
+//     // stop
+//     _navModule.stop();
+
+//     _display = "TURNING";
+
+//     // now turn until the line is found (left adjustment for right turn, right adjustment for left turn)
+//     if (isLeft)
+//     {
+//         _navModule.goRightWheel(120, false);
+//         _navModule.goLeftWheel(120, true);
+//         while (_lineMakerModule.getDetectionFlag() != LineMakerFlag::RIGHT_ADJUSTMENT)
+//         {
+//         }
+//     }
+//     else
+//     {
+//         _navModule.goRightWheel(120, true);
+//         _navModule.goLeftWheel(120, false);
+//         while (_lineMakerModule.getDetectionFlag() != LineMakerFlag::LEFT_ADJUSTMENT)
+//         {
+//         }
+//     }
+
+//     // stop
+//     _navModule.stop();
+
+//     _currentSequence = 0;
+//     _state = IdentifyCornerState::RECOGNIZE;
+// }
+
+// void IdentifyCorner::_matchSequence()
+// {
+//     _display = "MATCHING";
+//     _delay_ms(4000);
+//     // if the sequence is the same as the one we're looking for, then we found it
+//     if (_sequence == LCTH)
+//     {
+//         _display = "LCTH";
+//         _found = true;
+//     }
+//     else if (_sequence == LCBV)
+//     {
+//         _display = "LCBV";
+//         _found = true;
+//     }
+//     else if (_sequence == LCBH)
+//     {
+//         _display = "LCBH";
+//         _found = true;
+//     }
+//     else if (_sequence == LCTV)
+//     {
+//         _display = "LCTV";
+//         _found = true;
+//     }
+//     else if (_sequence == RCTH)
+//     {
+//         _display = "RCTH";
+//         _found = true;
+//     }
+//     else if (_sequence == RCTV)
+//     {
+//         _display = "RCTV";
+//         _found = true;
+//     }
+//     else if (_sequence == RCBV)
+//     {
+//         _display = "RCBV";
+//         _found = true;
+//     }
+//     else if (_sequence == RCBH)
+//     {
+//         _display = "RCBH";
+//         _found = true;
+//     }
+//     else
+//     {
+//         _display = "NOT FOUND";
+//         _found = false;
+//     }
+//     _delay_ms(4000);
+//     _state = IdentifyCornerState::GO_FORWARD;
+// }
+
+// void IdentifyCorner::_displayCurrentSequence()
+// {
+//     // display the current sequence
+//     char displayString[16];
+//     // sprintf(displayString, "%d", _currentSequence);
+//     //  use sprint f to format as bits display not decim
+
+//     // display in base 2 (binary) not decimal
+//     sprintf(displayString, "%d", _currentSequence);
+
+//     _display = displayString;
+// }
--- tp/projet/lib_dir/IdentifyCorner.h
+++ tp/projet/lib_dir/IdentifyCorner.h
@@ -8,15 +8,22 @@
 #include "lcm_so1602dtr_m_fw.h"
 
 
+// enum class IdentifyCornerState
+// {
+//     GO_FORWARD,
+//     DETECT_INTERSECTION,
+//     DETECT_FORWARD,
+//     TURN,
+//     RECOGNIZE,
+//     STOP
+// };
+
 enum class IdentifyCornerState
 {
     GO_FORWARD,
-    DETECT_INTERSECTION,
-    DETECT_FORWARD,
-    TURN,
-    RECOGNIZE,
-    STOP
-};
+    TURN_AROUND,
+    GO_BACK,
+}
 class IdentifyCorner
 {
 
@@ -31,38 +38,49 @@ private:
     LineMaker _lineMakerModule;
     LCM _display;
 
-    //sequence
-    uint16_t _sequence = 0b0000000000000000;
-    uint8_t _currentSequence;
-    uint8_t _bitshift = 0;
-    bool _found = false;
+    uint8_t _intersectionCount = 0;
 
-    // routines for each state
+    // routines
     void _goForward();
-    void _detectIntersection();
-    void _detectForward();
-    void _turn();
-    void _stop();
+    void _turnAround();
+    void _goBack();
 
-    void _displayCurrentSequence();
+    bool isRight = false;
 
-    // match the sequence
-    void _matchSequence();
+    void _displayCurrentIntersectionCount();
 
-    IdentifyCornerState _state = IdentifyCornerState::GO_FORWARD;
+    //sequence
+    // uint16_t _sequence = 0b0000000000000000;
+    // uint8_t _currentSequence;
+    // uint8_t _bitshift = 0;
+    // bool _found = false;
 
-    // define
-    static const uint8_t LEFT = 0b00000100;
-    static const uint8_t RIGHT = 0b00000001;
-    static const uint8_t FORWARD = 0b00000010;
+    // // routines for each state
+    // void _goForward();
+    // void _detectIntersection();
+    // void _detectForward();
+    // void _turn();
+    // void _stop();
 
-    // define sequence
-    static const uint16_t LCTH = 0b0000000100001011; // left corner top horizontal
-    static const uint16_t LCTV = 0b0000000000100011; // left corner top vertical
-    static const uint16_t LCBH = 0b0000000000111100; // left corner bottom horizontal
-    static const uint16_t LCBV = 0b0000000000110001; // left corner bottom vertical
-    static const uint16_t RCTH = 0b0000000000000110; // right corner top horizontal
-    static const uint16_t RCTV = 0b0000001001001011; // right corner top vertical
-    static const uint16_t RCBH = 0b0000000000011011; // right corner bottom horizontal
-    static const uint16_t RCBV = 0b0000000000110100; // right corner bottom vertical
+    // void _displayCurrentSequence();
+
+    // // match the sequence
+    // void _matchSequence();
+
+    // IdentifyCornerState _state = IdentifyCornerState::GO_FORWARD;
+
+    // // // define
+    // // static const uint8_t LEFT = 0b00000100;
+    // // static const uint8_t RIGHT = 0b00000001;
+    // // static const uint8_t FORWARD = 0b00000010;
+
+    // // // define sequence
+    // // static const uint16_t LCTH = 0b0000000100001011; // left corner top horizontal
+    // // static const uint16_t LCTV = 0b0000000000100011; // left corner top vertical
+    // // static const uint16_t LCBH = 0b0000000000111100; // left corner bottom horizontal
+    // // static const uint16_t LCBV = 0b0000000000110001; // left corner bottom vertical
+    // // static const uint16_t RCTH = 0b0000000000000110; // right corner top horizontal
+    // // static const uint16_t RCTV = 0b0000001001001011; // right corner top vertical
+    // // static const uint16_t RCBH = 0b0000000000011011; // right corner bottom horizontal
+    // // static const uint16_t RCBV = 0b0000000000110100; // right corner bottom vertical
 };
