ce92b7eb8d6b869f6571aa4ed4c8acbc14e19e9c
Jodel Alcindor <marcjodel.a@gmail.com>
2023-11-28 20:37:16 -0500

    cleaning

--- tp/projet/lib_dir/IdentifyCorner.h
+++ tp/projet/lib_dir/IdentifyCorner.h
@@ -78,39 +78,8 @@ private:
 
     void _displayCurrentIntersectionCount();
 
-    // sequence
-    //  uint16_t _sequence = 0b0000000000000000;
-    //  uint8_t _currentSequence;
-    //  uint8_t _bitshift = 0;
     bool _found = false;
     bool _blockIncrementation;
 
-    // // routines for each state
-    // void _goForward();
-    // void _detectIntersection();
-    // void _detectForward();
-    // void _turn();
-    // void _stop();
-
-    // void _displayCurrentSequence();
-
-    // // match the sequence
-    // void _matchSequence();
-
     IdentifyCornerState _state = IdentifyCornerState::GO_FORWARD_FIRST_LINE;
-
-    // // // define
-    // // static const uint8_t LEFT = 0b00000100;
-    // // static const uint8_t RIGHT = 0b00000001;
-    // // static const uint8_t FORWARD = 0b00000010;
-
-    // // // define sequence
-    // // static const uint16_t LCTH = 0b0000000100001011; // left corner top horizontal
-    // // static const uint16_t LCTV = 0b0000000000100011; // left corner top vertical
-    // // static const uint16_t LCBH = 0b0000000000111100; // left corner bottom horizontal
-    // // static const uint16_t LCBV = 0b0000000000110001; // left corner bottom vertical
-    // // static const uint16_t RCTH = 0b0000000000000110; // right corner top horizontal
-    // // static const uint16_t RCTV = 0b0000001001001011; // right corner top vertical
-    // // static const uint16_t RCBH = 0b0000000000011011; // right corner bottom horizontal
-    // // static const uint16_t RCBV = 0b0000000000110100; // right corner bottom vertical
 };
