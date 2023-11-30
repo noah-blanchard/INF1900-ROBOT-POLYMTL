4805c76aee15152511a71d5d25e11f8f2520eee1
noah-blanchard <matanaworgen@gmail.com>
2023-11-30 14:21:23 -0500

    ok

--- tp/projet/lib_dir/Navigation.cpp
+++ tp/projet/lib_dir/Navigation.cpp
@@ -772,10 +772,10 @@ void Navigation::_turnRight()
             // if we detect the line on the left, it means we met the line
             // so stop moving and go to forward state
             stop();
+            *_currentOrientation = _nextMoveValue.orientation;
             _delay_ms(1000);
             _preventInitForward = true;
-                _chooseForwardMove();
-            
+            _chooseForwardMove();
 
             break;
         }
@@ -816,11 +816,11 @@ void Navigation::_turnLeft()
         {
             // if we detect the line on the left, it means we met the line
             // so stop moving and go to forward state
-           stop();
+            stop();
+            *_currentOrientation = _nextMoveValue.orientation;
             _delay_ms(1000);
             _preventInitForward = true;
-                _chooseForwardMove();
-            
+            _chooseForwardMove();
 
             break;
         }
