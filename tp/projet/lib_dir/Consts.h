c6e92efc2ac9bc9746965b97701f0dc61fb5d11d
noah-blanchard <matanaworgen@gmail.com>
2023-12-02 23:07:21 -0500

    ok

--- tp/projet/lib_dir/Consts.h
+++ tp/projet/lib_dir/Consts.h
@@ -3,13 +3,13 @@
 
 // WHEEL SPEED CONSTANTS
 const uint8_t BASE_WHEEL_SPEED = 90;
-const uint8_t BACK_WHEEL_SPEED = 75;
+const uint8_t BACK_WHEEL_SPEED = 80;
 const uint8_t TURN_WHEEL_SPEED = 70;
-const uint8_t LINE_ADJUST_OFFSET = 10;
+const uint8_t LINE_ADJUST_OFFSET = 15;
 const uint8_t OFFSET = 2;
 
 // NAVIGATION DELAY CONSTANTS
-const uint16_t BASE_ADJ_FWD_AMT = 140;
+const uint16_t BASE_ADJ_FWD_AMT = 120;
 const uint16_t LESS_ADJ_FWD_AMT = 80;
 const uint16_t BASE_FWD_DELAY_AMT = 100;
 const uint8_t ADJUST_DELAY = 10;
